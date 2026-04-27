7ea24b70c4a48fd1d8b3e8643c8afc21361bbe57
tuanh10082007-glitch <tuanh10082007@gmail.com>
2026-04-27 23:28:12 +0700

    Add files via upload

---
+++ DONGVAT723.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float money, total = 0;
+
+    do {
+        printf("\n===== QUAN LY CHI TIEU =====\n");
+        printf("1. Them chi tieu\n");
+        printf("2. Xem tong chi tieu\n");
+        printf("0. Thoat\n");
+        printf("Chon: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            printf("Nhap so tien: ");
+            scanf("%f", &money);
+            total = total + money;
+        }
+        else if (choice == 2) {
+            printf("Tong chi tieu: %.2f\n", total);
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
